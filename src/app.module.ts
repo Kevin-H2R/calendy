4d8395f96c7e64ac28888c09f5390c64f73ce167
Kevin <herrbach.kevin@gmail.com>
2022-06-24 10:40:21 +0900

    Adds basic resource code for events and templates

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { User } from './users/entities/user.entity';
+import { TemplatesModule } from './templates/templates.module';
+import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3309,
+    username: 'root',
+    password: 'root',
+    database: 'calendy',
+    entities: [User],
+    synchronize: true,
+  }), TemplatesModule, EventsModule,],
   controllers: [AppController],
   providers: [AppService],
 })
